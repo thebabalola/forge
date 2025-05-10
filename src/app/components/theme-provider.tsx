00b3a03e90a855f616b3fa1e1d8f0e4ca5bc5d03
kemsguy7 <mattidungafa@gmail.com>
2025-05-10 16:08:21 +0100

    updates: updated imports for theme provider

--- src/app/components/theme-provider.tsx
+++ src/app/components/theme-provider.tsx
@@ -1,7 +1,10 @@
 'use client';
 
 import * as React from 'react';
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from 'next-themes';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+
+// More generic props type that should work without specific imports
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
