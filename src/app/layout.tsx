423091100cc57950c1a47e5632a6d54b78f8bd21
kemsguy7 <mattidungafa@gmail.com>
2025-05-11 21:37:10 +0100

    updated app/layout.tsx to fix build error and importing fonts manually in globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,8 @@
-import { Inter } from 'next/font/google';
-import { Poppins } from 'next/font/google';
 import './globals.css';
 import Header from './components/layout/Header';
 import { headers } from 'next/headers';
 import ContextProvider from '../contexts/appKitContext';
 import { WalletProvider } from '../contexts/WalletContext';
-
-// import { config } from '../lib/wagmi-config';
-// import { useState } from 'react';
-
-const inter = Inter({ subsets: ['latin'] });
-
-const poppinsFont = Poppins({
-  weight: ['400', '500', '700'],
-  subsets: ['latin'],
-  variable: '--font-poppins',
-});
-
-// const metadata = {
-//   title: 'ProptyChain - Blockchain-Secured Real Estate in Nigeria',
-//   description:
-//     'Buy, sell, rent and lease properties in Nigeria with blockchain security and smart contracts.',
-// };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const headersObj = await headers();
@@ -29,10 +10,10 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
   return (
     <html lang='en'>
-      <body className={`${inter.className} ${poppinsFont.variable} min-h-screen bg-black`}>
+      <body className='min-h-screen bg-black'>
         <Header />
         <ContextProvider cookies={cookies}>
-          <WalletProvider>{children} </WalletProvider>
+          <WalletProvider>{children}</WalletProvider>
         </ContextProvider>
       </body>
     </html>
