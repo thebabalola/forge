2d1bc6e8693c107ea2a485f3fe41a673ce9343af
thebabalola <t.babalolajoseph@gmail.com>
2026-01-30 02:53:43 +0100

    chore(frontend): finalize VaultAnalytics minor edits

--- frontend/components/vaults/index.ts
+++ frontend/components/vaults/index.ts
@@ -1,2 +1,2 @@
-export { default as VaultAnalytics } from './VaultAnalytics';
-export * from './VaultAnalytics';
+export { default as VaultAnalytics } from "./VaultAnalytics";
+export * from "./VaultAnalytics";
